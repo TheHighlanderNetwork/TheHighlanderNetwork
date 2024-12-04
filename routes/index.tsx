ab15881a5ab2f0b5abdcb5c431a0c83d5a38700b
Tech13-08 <falaktulsi@gmail.com>
2024-12-04 12:55:27 -0800

    Simple landing page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,63 @@
-import { useSignal } from "@preact/signals";
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
-  const count = useSignal(3);
   return (
-    <div class="px-4 py-8 mx-auto bg-[#86efac]">
+    <div class="px-4 py-8 w-screen h-screen bg-grey-light">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
-        <Counter count={count} />
+        <div class="flex flex-row text-4xl font-bold font-display text-yellow">
+          <h1>The Highlander&nbsp;</h1>
+          <h1 class="text-blue">Network</h1>
+        </div>
+        <div class="flex flex-row text-4xl font-bold font-display text-black">
+          <div>
+            <img
+              class="my-6"
+              src="/classes.svg"
+              width="128"
+              height="128"
+              alt="Classes logo"
+            />
+            <h2>Classes</h2>
+          </div>
+          <div>
+            <img
+              class="my-6"
+              src="/professors.svg"
+              width="128"
+              height="128"
+              alt="Professors logo"
+            />
+            <h2>Professors</h2>
+          </div>
+          <div>
+            <img
+              class="my-6"
+              src="/businesses.svg"
+              width="128"
+              height="128"
+              alt="Businesses logo"
+            />
+            <h2>Businesses</h2>
+          </div>
+          <div>
+            <img
+              class="my-6"
+              src="/organizations.svg"
+              width="128"
+              height="128"
+              alt="Organizations logo"
+            />
+            <h2>Organizations</h2>
+          </div>
+          <div>
+            <img
+              class="my-6"
+              src="/housing.svg"
+              width="128"
+              height="128"
+              alt="Housing logo"
+            />
+            <h2>Housing</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,18 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  theme: {
+    colors: {
+      "yellow": "#FDB913",
+      "blue": "#0074BC",
+      "grey-light": "#EFEDED",
+      "grey-dark": "#323232",
+      "black": "#000000",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+      display: ["Oswald"],
+    },
+  },
 } satisfies Config;
