db5309f6293ce58920f6a052dfb563693093e9d7
Pranay Thakur <pranaythakur543@gmail.com>
2025-03-12 02:23:55 -0700

    (feat) make header better as well as fix linting issues Improve the header and make it more interactive. Went above and beyond and added separate states for when a student or business is logged out the option to log in or sign up. At the very least if the student is logged in the username shows up automatically. The student or business can then hover over their username and have the option to actually log out or even change their personal settings. More options can be added as the application constantly is getting scaled up. By doing this we remove the need for individual Log In and Sign Up buttons on every page and everything can just use the "UsernameHeader" island. This also deprecates the "HeaderSignUpButton" island, (whose deprecation would be another PR). As well improves the overall consistency of the codebase as the Sign In button doesn't have its own island.

--- islands/UsernameHeader.tsx
+++ islands/UsernameHeader.tsx
@@ -28,12 +28,14 @@ export default function UsernameHeader() {
     return (
       <div className="flex gap-2">
         <button
+          type="button"
           onClick={() => (globalThis.location.href = "/login")}
           className="bg-blue text-white rounded-md px-4 py-2 text-sm font-medium"
         >
           Log In
         </button>
         <button
+          type="button"
           onClick={() => (globalThis.location.href = "/signup")}
           className="bg-yellow text-white rounded-md px-4 py-2 text-sm font-medium"
         >
@@ -64,6 +66,7 @@ export default function UsernameHeader() {
             User Settings
           </button>
           <button
+            type="button"
             onClick={handleLogout}
             className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-grey-light"
           >
