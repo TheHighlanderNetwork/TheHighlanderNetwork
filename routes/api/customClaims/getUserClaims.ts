90fa77233a4d48855036ed9f5bf8eb3009d158cc
dzshao <danielzhshao@gmail.com>
2025-03-17 00:24:16 -0700

    Added handlers to verify an ID token, and created util function to verify token in front end. Business and Google SignIn verify token after signing in

--- routes/api/customClaims/getUserClaims.ts
+++ routes/api/customClaims/getUserClaims.ts
@@ -1,6 +1,9 @@
 import { auth } from "../firebaseAdmin.ts";
+import { Handlers } from "$fresh/server.ts";
 
-async function getUserClaims(uid: string): Promise<Record<string, unknown> | undefined> {
+export async function getUserClaims(
+  uid: string,
+): Promise<Record<string, unknown> | undefined> {
   try {
     // Retrieve custom claims from Firebase
     const user = await auth.getUser(uid);
@@ -16,4 +19,49 @@ async function getUserClaims(uid: string): Promise<Record<string, unknown> | und
   }
 }
 
-getUserClaims("kLwjp4lZxPgR5UJgZ4nerSqT7xS2");
+export const handler: Handlers = {
+  // Retrieve a user's role
+  async GET(req) {
+    try {
+      const { uid } = await req.json(); // Only accept UID
+
+      console.log("Received request to retrieve role for user: ", uid);
+
+      // Return error if no uid is included
+      if (!uid) {
+        return new Response(
+          JSON.stringify({ success: false, error: "UID is required" }),
+          { status: 400 },
+        );
+      }
+
+      const role = await getUserClaims(uid);
+
+      return new Response(
+        JSON.stringify({ success: true, role: role }),
+        {
+          status: 200,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        return new Response(
+          JSON.stringify({ success: false, error: error.message }),
+          {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          },
+        );
+      } else {
+        return new Response(
+          JSON.stringify({ success: false, error: error }),
+          {
+            status: 500,
+            headers: { "Content-Type": "application/json" },
+          },
+        );
+      }
+    }
+  },
+};
--- routes/api/customClaims/setUserClaims.ts
+++ routes/api/customClaims/setUserClaims.ts
@@ -1,6 +1,5 @@
 import { auth, db } from "../firebaseAdmin.ts";
 import { Handlers } from "$fresh/server.ts";
-import { QueryDocumentSnapshot } from "npm:firebase-admin/firestore"
 
 async function setUserClaims(uid: string, claims: Record<string, unknown>) {
   try {
@@ -10,8 +9,51 @@ async function setUserClaims(uid: string, claims: Record<string, unknown>) {
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error(error.message);
+      throw error;
     } else {
       console.error("An unknown error occurred", error);
+      throw new Error("Unknown");
+    }
+  }
+}
+
+export async function autoSetRole(uid: string): Promise<string> {
+  try {
+    // Fetch user details from Firebase
+    console.log("Retrieving user record...");
+    const userRecord = await auth.getUser(uid);
+    console.log("Retrieved");
+    const email = userRecord.email || "";
+
+    let newRole = "business"; // Role for non-students
+
+    // Set role to student for @ucr.edu emails
+    if (email?.endsWith("@ucr.edu")) {
+      newRole = "student";
+    }
+
+    // Assign admin role only if email is an admin email
+    console.log("Checking admin doc...");
+    const adminDoc = await db.collection("admins").get();
+    console.log("Received admin doc...");
+
+    for (const doc of adminDoc.docs) {
+      if (email === doc.data().email) {
+        newRole = "admin";
+        break;
+      }
+    }
+
+    // Set custom claim
+    await setUserClaims(uid, { role: newRole });
+    return newRole;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error(error.message);
+      throw error;
+    } else {
+      console.error("An unknown error occurred", error);
+      throw new Error("Unknown");
     }
   }
 }
@@ -32,32 +74,7 @@ export const handler: Handlers = {
         );
       }
 
-      // Fetch user details from Firebase
-      console.log("Retrieving user record...");
-      const userRecord = await auth.getUser(uid);
-      console.log("Retrieved");
-      const email = userRecord.email || "";
-
-      let newRole = "business"; // Role for non-students
-
-      // Set role to student for @ucr.edu emails
-      if (email?.endsWith("@ucr.edu")) {
-        newRole = "student";
-      }
-
-      // Assign admin role only if email is an admin email
-      console.log("Checking admin doc...");
-      const adminDoc = await db.collection("admins").get();
-      console.log("Received admin doc...");
-      adminDoc.forEach((doc: QueryDocumentSnapshot) => {
-        console.log(doc.data());
-        if (email == doc.data().email) {
-          newRole = "admin";
-        }
-      });
-
-      // Set custom claim securely
-      await setUserClaims(uid, { role: newRole });
+      const newRole = await autoSetRole(uid);
 
       return new Response(
         JSON.stringify({ success: true, message: `Role ${newRole} assigned` }),
